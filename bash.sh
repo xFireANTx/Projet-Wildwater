250b96332306773db8333d91e9808941259b4548
MoT186 <tommachefert78240@gmail.com>
2025-12-16 23:18:49 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
+##Le processus des 3 fonctions servant à générer les fichiers histo.dat sont similaire. On remarque que chaque histo demandé représente une étape du circuit et que selon 
+##l'étape, on a un motif différent de '-';txt;'-'etc. Ainsi, on effectue un filtrage en colonne pour récupérer les informations dont on a besoin et on trie le tout
+##Finalement on effectue les opérations nécessaire en c sur le fichier triés et filtrés
+
 ##On élimine les lignes dont la colonnes 3 n'est pas vide ('-')(pas une usine), on récupère les colonnes 2 et 4 car elles contiennent les usines avec vol_trait_max, on les trie
-##TODO supprimer les lignes qui ne sont pas des usines
 volume_max(){
+#TODO convertir en million de m3/an (en c ou en shell si possible) (optionnelle créer un unique fichier.c qui regroupe les 2 somme car très similaire)
 	local source="$1"
 	awk -F';' '$3 == "-"' "$source" |cut -d';' -f2,4 | sort -t';' -k1,1 -d -r
 }
 
 volume_source(){
 	local source="$1"
-	awk -F';' '$1 == "-"' "$source" | awk -F';' '$3 != "-"' | awk -F';' '$4 !="-"' | cut -d';' -f3,4 |sort -t';' -k1,1 -d -r > temp
+	awk -F';' '$1 == "-"' "$source" | awk -F';' '$3 != "-"' | awk -F';' '$4 !="-"' | cut -d';' -f3,4 |sort -t';' -k1,1 -d -r > temp_source
+	
+	gcc -o exe somme_source.c
+	./exe temp_source out_source
+	
+	cat out_source
+	rm temp_source
+	rm out_source
 }
 
 volume_reel(){
-	return 0
+	local source="$1"
+	awk -F';' '$1 == "-"' "$source" | awk -F';' '$3 != "-"' | awk -F';' '$4 !="-"' | cut -d';' -f3-5 |sort -t';' -k1,1 -d -r > temp_reel
+	
+	gcc -o exe somme_reel.c
+	./exe temp_reel out_reel
+	
+	cat out_reel
+	rm temp_reel
+	rm out_reel
 }
 
 ##Vérification du bon nombre d'argument et du bon format de ceux-ci
@@ -23,7 +42,7 @@ then
 	exit 1
 fi
 
-
+#TODO ajoutez le choix leaks pour le deuxième argument
 fichier_source=$1
 histo=$2
 mode=$3
@@ -59,4 +78,3 @@ echo "$donnee" >> "$Nom_fichier"
 
 echo "Nom du fichier créé: $Nom_fichier"
 cat $Nom_fichier
-#TODO écrire les 3 fonctions des usines selon le max volume,source volume,reel volume (pas forcément)
