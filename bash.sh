ab8a0d7e4d23c6024f76db5eac300729c5263219
MoT186 <tommachefert78240@gmail.com>
2025-12-16 11:39:44 +0100

    Refactor volume functions and update logic

--- bash.sh
+++ bash.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-##On récupère les colonnes 2 et 4 car elles contiennent les usines avec vol_trait_max, on supprime les lignes qui ne possèdent rien ('-') sur la deuxième colonnne puis on les trie
+##On élimine les lignes dont la colonnes 3 n'est pas vide ('-')(pas une usine), on récupère les colonnes 2 et 4 car elles contiennent les usines avec vol_trait_max, on les trie
 ##TODO supprimer les lignes qui ne sont pas des usines
 volume_max(){
 	local source="$1"
-	cut -d';' -f2,4  "$source" | awk -F';' '$2 != "-"'| sort -t';' -k1,1 -d -r
+	awk -F';' '$3 == "-"' "$source" |cut -d';' -f2,4 | sort -t';' -k1,1 -d -r
 }
 
 volume_source(){
-	return 0
+	local source="$1"
+	awk -F';' '$1 == "-"' "$source" | awk -F';' '$3 != "-"' | awk -F';' '$4 !="-"' | cut -d';' -f3,4 |sort -t';' -k1,1 -d -r > temp
 }
 
 volume_reel(){
@@ -40,8 +41,12 @@ case $mode in
 'max')  Nom_colonne="volume max(M.m3/an)" 
 	donnee=$(volume_max "$fichier_source")
 	;;
-'src')  Nom_colonne="volume source(M.m3/an)" ;;
-'real') Nom_colonne="volume reel(M.m3/an)" ;;
+'src')  Nom_colonne="volume source(M.m3/an)" 
+	donnee=$(volume_source "$fichier_source")
+	;;
+'real') Nom_colonne="volume reel(M.m3/an)" 
+	donnee=$(volume_reel "$fichier_source")
+	;;
 *) echo "Commande $mode incorrecte, 'max' ou 'src' ou 'real'sont attendue en 3ème argument." 
 	exit 1
 	;;
@@ -55,5 +60,3 @@ echo "$donnee" >> "$Nom_fichier"
 echo "Nom du fichier créé: $Nom_fichier"
 cat $Nom_fichier
 #TODO écrire les 3 fonctions des usines selon le max volume,source volume,reel volume (pas forcément)
-
-
