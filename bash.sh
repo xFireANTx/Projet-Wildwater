29cf8c1756e3ad99a9197faf0d76a9588a656143
MoT186 <tommachefert78240@gmail.com>
2025-12-17 11:03:11 +0100

    Add support for 'leaks' argument in bash script
    Updated the script to handle a new argument 'leaks' and modified the logic accordingly. Adjusted file naming and output handling based on the new argument.

--- bash.sh
+++ bash.sh
@@ -4,7 +4,6 @@
 ##l'étape, on a un motif différent de '-';txt;'-'etc. Ainsi, on effectue un filtrage en colonne pour récupérer les informations dont on a besoin et on trie le tout
 ##Finalement on effectue les opérations nécessaire en c sur le fichier triés et filtrés
 
-##On élimine les lignes dont la colonnes 3 n'est pas vide ('-')(pas une usine), on récupère les colonnes 2 et 4 car elles contiennent les usines avec vol_trait_max, on les trie
 volume_max(){
 	local source="$1"
 	awk -F';' '$3 == "-"' "$source" |cut -d';' -f2,4 | sort -t';' -k1,1 -d -r > temp_volume
@@ -14,8 +13,6 @@ volume_max(){
 	
 	cat out_volume
 	rm temp_volume out_volume
-
-	
 }
 
 volume_source(){
@@ -26,8 +23,7 @@ volume_source(){
 	./exe temp_source out_source
 	
 	cat out_source
-	rm temp_source
-	rm out_source
+	rm temp_source  out_source
 }
 
 volume_reel(){
@@ -38,50 +34,52 @@ volume_reel(){
 	./exe temp_reel out_reel
 	
 	cat out_reel
-	rm temp_reel
-	rm out_reel
+	rm temp_reel out_reel
 }
 
 ##Vérification du bon nombre d'argument et du bon format de ceux-ci
 if [ $# -ne 3 ] 
 then
-	echo "Pas le bon nombre d'arguments, 3 arguments sont attendues (1: 'nom_fichier, 2: 'histo', 3: 'max' ou 'src' ou 'real')."
+	echo "Pas le bon nombre d'arguments, 3 arguments sont attendues (1: 'nom_fichier, 2: 'histo'/'leaks', 3: 'max' ou 'src' ou 'real'/'Identifiant_usine')."
 	exit 1
 fi
 
-#TODO ajoutez le choix leaks pour le deuxième argument
 fichier_source=$1
-histo=$2
+histo_leaks=$2
 mode=$3
 
-if [ "$histo" != "histo" ] 
-then
-	echo "Commande $nom_argument2 incorrecte, 'histo' est attendue en 2ème argument."
-	exit 1
-fi
 
-Nom_fichier="histo_${mode}.dat"
+Nom_fichier="${histo_leaks}_${mode}.dat"
+case $histo_leaks in
+'histo')
+	##On récupère dans données les informations du fichier trié dans le bon ordre selon le 3ème argument entré par l'utilisateur
+	case $mode in 
+	'max')  Nom_colonne="volume max(M.m3/an)" 
+		donnee=$(volume_max "$fichier_source")
+		;;
+	'src')  Nom_colonne="volume source(M.m3/an)" 
+		donnee=$(volume_source "$fichier_source")
+		;;
+	'real') Nom_colonne="volume reel(M.m3/an)" 
+		donnee=$(volume_reel "$fichier_source")
+		;;
+	*) 	echo "Commande $mode incorrecte, 'max' ou 'src' ou 'real'sont attendue en 3ème argument." 
+		exit 1
+		;;
+	esac
+	echo -n "identifier;" > "$Nom_fichier" 
+	echo "$Nom_colonne" >> "$Nom_fichier"
+;;
 
-##On récupère dans données les informations du fichier trié dans le bon ordre selon le 3ème argument entré par l'utilisateur
-case $mode in 
-'max')  Nom_colonne="volume max(M.m3/an)" 
-	donnee=$(volume_max "$fichier_source")
+'leaks')
+	donnee = "empty"
 	;;
-'src')  Nom_colonne="volume source(M.m3/an)" 
-	donnee=$(volume_source "$fichier_source")
-	;;
-'real') Nom_colonne="volume reel(M.m3/an)" 
-	donnee=$(volume_reel "$fichier_source")
-	;;
-*) echo "Commande $mode incorrecte, 'max' ou 'src' ou 'real'sont attendue en 3ème argument." 
+*) 	echo "Commande $histo_leaks incorrecte, 'histo' ou 'leaks sont attendus en 2ème argument."
 	exit 1
 	;;
 esac
 
 ##On met dans notre fichier contenu sur le DD toutes les données obtenues plus tôt
-echo -n "identifier;" > "$Nom_fichier" 
-echo "$Nom_colonne" >> "$Nom_fichier"
-echo "$donnee" >> "$Nom_fichier"
 
+echo "$donnee" >> "$Nom_fichier"
 echo "Nom du fichier créé: $Nom_fichier"
-cat $Nom_fichier
