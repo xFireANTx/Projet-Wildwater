96317a721beefaa5385695dc0d95cd4610f58426
MoT186 <tommachefert78240@gmail.com>
2025-12-20 18:11:13 +0100

    Improve argument handling and function execution
    Refactor argument verification and streamline function calls.

--- bash.sh
+++ bash.sh
@@ -1,82 +1,82 @@
 #!/bin/bash
 
-##Le processus des 3 fonctions servant à générer les fichiers histo.dat sont similaire. On remarque que chaque histo demandé représente une étape du circuit et que selon 
-##l'étape, on a un motif différent de '-';txt;'-'etc. Ainsi, on effectue un filtrage en colonne pour récupérer les informations dont on a besoin
-##Finalement on effectue les opérations nécessaire en c sur le fichier triés et filtrés
-
 volume_max(){
-	local source="$1"
-	awk -F';' '$3 == "-"' "$source" |cut -d';' -f2,4  > temp_volume
-	
-	./exe temp_volume out_volume histo max
-	
-	cat out_volume
-	rm temp_volume out_volume
+    local source="$1"
+    awk -F';' '$3 == "-"' "$source" | cut -d';' -f2,4 > temp_volume
+    ./exe "$1" temp_volume out_volume histo max
+    cat out_volume
+    rm temp_volume out_volume
 }
 
 volume_source(){
-	local source="$1"
-	awk -F';' '$1 == "-"' "$source" | awk -F';' '$3 != "-"' | awk -F';' '$4 !="-"' | cut -d';' -f3,4  > temp_source
-	
-	./exe temp_source out_source histo src
-	
-	cat out_source
-	rm temp_source  out_source
+    local source="$1"
+    awk -F';' '$1 == "-" && $3 != "-" && $4 != "-"' "$source" | cut -d';' -f3,4 > temp_source
+    ./exe "$1" temp_source out_source histo src
+    cat out_source
+    rm temp_source out_source
 }
 
 volume_reel(){
-	local source="$1"
-	awk -F';' '$1 == "-"' "$source" | awk -F';' '$3 != "-"' | awk -F';' '$4 !="-"' | cut -d';' -f3-5  > temp_reel
-	
-	./exe temp_reel out_reel histo reel
-	
-	cat out_reel
-	rm temp_reel out_reel
+    local source="$1"
+    awk -F';' '$1 == "-" && $3 != "-" && $4 != "-"' "$source" | cut -d';' -f3-5 > temp_reel
+    ./exe "$1" temp_reel out_reel histo reel
+    cat out_reel
+    rm temp_reel out_reel
 }
 
-##Vérification du bon nombre d'argument et du bon format de ceux-ci
-if [ $# -ne 3 ] 
-then
-	echo "Pas le bon nombre d'arguments, 3 arguments sont attendues (1: 'nom_fichier, 2: 'histo'/'leaks', 3: 'max' ou 'src' ou 'reel'/'Identifiant_usine')."
-	exit 1
+# Vérification arguments
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 fichier histo|leaks max|src|reel"
+    exit 1
 fi
 
-fichier_source=$1
-histo_leaks=$2
-mode=$3
-
+fichier_source="$1"
+histo_leaks="$2"
+mode="$3"
 
 Nom_fichier="${histo_leaks}_${mode}.dat"
-case $histo_leaks in
-'histo')
-	##On récupère dans données les informations du fichier trié dans le bon ordre selon le 3ème argument entré par l'utilisateur
-	case $mode in 
-	'max')  Nom_colonne="volume max(M.m3/an)" 
-		donnee=$(volume_max "$fichier_source")
-		;;
-	'src')  Nom_colonne="volume source(M.m3/an)" 
-		donnee=$(volume_source "$fichier_source")
-		;;
-	'reel') Nom_colonne="volume reel(M.m3/an)" 
-		donnee=$(volume_reel "$fichier_source")
-		;;
-	*) 	echo "Commande $mode incorrecte, 'max' ou 'src' ou 'reel'sont attendue en 3ème argument." 
-		exit 1
-		;;
-	esac
-	echo -n "identifier;" > "$Nom_fichier" 
-	echo "$Nom_colonne" >> "$Nom_fichier"
-;;
 
-'leaks')
-	donnee = "empty"
-	;;
-*) 	echo "Commande $histo_leaks incorrecte, 'histo' ou 'leaks sont attendus en 2ème argument."
-	exit 1
-	;;
+case "$histo_leaks" in
+histo)
+    case "$mode" in
+    max)
+        Nom_colonne="volume max(M.m3/an)"
+        donnee=$(volume_max "$fichier_source")
+        ;;
+    src)
+        Nom_colonne="volume source(M.m3/an)"
+        donnee=$(volume_source "$fichier_source")
+        ;;
+    reel)
+        Nom_colonne="volume reel(M.m3/an)"
+        donnee=$(volume_reel "$fichier_source")
+        ;;
+    *)
+        echo "Mode invalide"
+        exit 1
+        ;;
+    esac
+    ;;
+leaks)
+    Nom_colonne="leaks_volume(M.m3/an)"
+    donnee="empty"
+    ;;
+*)
+    echo "Type invalide"
+    exit 1
+    ;;
 esac
 
-##On met dans notre fichier contenu sur le DD toutes les données obtenues plus tôt
-
+echo "identifier;$Nom_colonne" > "$Nom_fichier"
 echo "$donnee" >> "$Nom_fichier"
+
+{
+    head -n 1 "$Nom_fichier"
+    tail -n +2 "$Nom_fichier" | sort -t';' -k2,2 -n -r
+} > temp_val
+
+head -n 11 temp_val| tail -n 10 > temp_max
+tail -n 50 temp_val > temp_min
+rm temp_val
+
 echo "Nom du fichier créé: $Nom_fichier"
