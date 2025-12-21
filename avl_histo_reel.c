39292dfebbd077d1cb9c4d3e3ad5f4087e9ce3d2
MoT186 <tommachefert78240@gmail.com>
2025-12-21 16:14:07 +0100

    Update avl_histo_reel.c

--- avl_histo_reel.c
+++ avl_histo_reel.c
@@ -7,6 +7,18 @@ int max_reel(int a, int b){
 	return (a > b)? a:b ;
 }
 
+int hauteur_reel(Volume_reel *n){
+    if(n == NULL) return 0;
+    return n->hauteur;
+}
+
+int facteur_equilibre_reel(Volume_reel *n){
+    if (n == NULL)
+        return 0;
+
+    return hauteur_reel(n->fg) - hauteur_reel(n->fd);
+}
+
 Volume_reel* create_vol_reel(char *id, double volume, double fuite){
 	if(id == NULL){
 		printf("vol_reel: id n'est pas définit\n");
@@ -33,46 +45,6 @@ Volume_reel* create_vol_reel(char *id, double volume, double fuite){
 	nouv->hauteur = 1;
 	
 	return nouv;
-}
-
-Volume_reel* ajouter_vol_reel(Volume_reel* arbre, char* id, double volume, double fuite){
-
-	if(id == NULL){
-		printf("ajou_vol_reel: Erreur id vide\n"); 
-		exit(1);
-	}
-	
-	if(arbre == NULL){
-		return create_vol_reel(id, volume, fuite);
-	}
-	
-	int comp_id = strcmp(arbre->identifiant,id);
-	
-	if(!comp_id){
-		arbre->volume += (volume*(1-(fuite/100.0)) / 1000.0);
-	}
-	else if(comp_id < 0){
-		arbre->fg = ajouter_vol_reel(arbre->fg, id, volume, fuite);
-	}
-	else {
-		arbre->fd = ajouter_vol_reel(arbre->fd, id, volume, fuite);
-	}
-	return equilibrer_reel(arbre); 
-}
-
-
-void infixe_reel_inverse(Volume_reel* arbre, FILE* sortie){
-	if(arbre == NULL){
-		return;
-	}
-	infixe_reel_inverse(arbre->fg,sortie);
-	fprintf(sortie,"%s;%lf\n",arbre->identifiant,arbre->volume);
-	infixe_reel_inverse(arbre->fd,sortie);
-}
-
-int hauteur_reel(Volume_reel *n){
-    if(n == NULL) return 0;
-    return n->hauteur;
 }
 
 Volume_reel *rotation_droite_reel(Volume_reel *a){
@@ -111,20 +83,10 @@ Volume_reel *rotation_gauche_droite_reel(Volume_reel *a){
     return rotation_droite_reel(a);
 }
 
-
-
-
-int facteur_equilibre_reel(Volume_reel *n){
-    if (n == NULL)
-        return 0;
-
-    return hauteur_reel(n->fg) - hauteur_reel(n->fd);
-}
-
 Volume_reel *equilibrer_reel(Volume_reel *n){
     if (n == NULL)
         return NULL;
-    n->hauteur = 1 + max_reel(hauteur_reel(n->fg), hauteur_reel(n->fd));
+	
     int balance = facteur_equilibre_reel(n);
 
     if (balance > 1 && facteur_equilibre_reel(n->fg) >= 0){
@@ -132,7 +94,7 @@ Volume_reel *equilibrer_reel(Volume_reel *n){
     }
 
     if (balance > 1 && facteur_equilibre_reel(n->fg) < 0){
-    return rotation_gauche_droite_reel(n);
+    	return rotation_gauche_droite_reel(n);
     }
 
     if (balance < -1 && facteur_equilibre_reel(n->fd) <= 0){
@@ -144,3 +106,45 @@ Volume_reel *equilibrer_reel(Volume_reel *n){
 
     return n; // déjà équilibré
 }
+
+
+Volume_reel* ajouter_vol_reel(Volume_reel* arbre, char* id, double volume, double fuite){
+
+	if(id == NULL){
+		printf("ajou_vol_reel: Erreur id vide\n"); 
+		exit(1);
+	}
+	
+	if(arbre == NULL){
+		return create_vol_reel(id, volume, fuite);
+	}
+	
+	int comp_id = strcmp(arbre->identifiant,id);
+	
+	if(!comp_id){
+		arbre->volume += (volume*(1-(fuite/100.0)) / 1000.0);
+		return arbre;
+	}
+	else if(comp_id < 0){
+		arbre->fg = ajouter_vol_reel(arbre->fg, id, volume, fuite);
+	}
+	else {
+		arbre->fd = ajouter_vol_reel(arbre->fd, id, volume, fuite);
+	}
+
+	arbre->hauteur = 1 + max_reel(hauteur_reel(arbre->fg), hauteur_reel(arbre->fd));
+	return equilibrer_reel(arbre); 
+}
+
+
+void infixe_reel_inverse(Volume_reel* arbre, FILE* sortie){
+	if(arbre == NULL){
+		return;
+	}
+	infixe_reel_inverse(arbre->fg,sortie);
+	fprintf(sortie,"%s;%lf\n",arbre->identifiant,arbre->volume);
+	infixe_reel_inverse(arbre->fd,sortie);
+}
+
+
+
