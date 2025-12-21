710182fd168552bbe620a59c6f67e60f9fe121e1
MoT186 <tommachefert78240@gmail.com>
2025-12-21 16:26:47 +0100

    Update basic_functions.h

--- basic_functions.h
+++ basic_functions.h
@@ -1,58 +1,58 @@
-#ifndef BASIC_FUNCTIONS_H_  
+#ifndef BASIC_FUNCTIONS_H_
 #define BASIC_FUNCTIONS_H_
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//arbres
 
-//traitement
 #define CODE_SIZE 11
-typedef struct Infra{
+
+typedef struct Infra {
     int type;
     char code_usine[CODE_SIZE];
     char code_precedent[CODE_SIZE];
     char code_actuel[CODE_SIZE];
     float fuite;
-    float flux; // pas besoin de capa max
-}infra;
+    float flux;
+} infra;
 
-typedef struct Arbres_fuites{
+typedef struct Arbres_fuites {
     infra *structure;
-    struct Arbres_fuites *suivant;
-    struct Arbres_fuites *actuelf;
-}arbres_fuites;
+    struct Arbres_fuites *suivant;  
+    struct Arbres_fuites *actuelf;  
+} arbres_fuites;
 
-typedef struct Racine{
+typedef struct Racine {
     char code_usine[CODE_SIZE];
     float flux;
-    struct Racine *suivant;
-    struct Arbres_fuites *actuelf;
-}racine;
+    struct Racine *suivant;         
+    struct Arbres_fuites *actuelf;  
+} racine;
 
-typedef struct Arbre{
-    racine *usine; //pour trier l arbre utilise arbre->racine->code usine       
+typedef struct Arbre {
+    racine *usine;
     struct Arbre *fg;
     struct Arbre *fd;
     int hauteur;
-}arbre;
+} arbre;
 
-infra *remplir_infra(char *line, int type);         //marche pour 3,4,5,6 jsp si je veut utiliser la meme chose pour usine et source n est pas utile
+
+infra *remplir_infra(char *line, int type);
 arbres_fuites *createNode(char *line, int type);
+
+
 arbres_fuites *addChildfuites(arbres_fuites *parent, arbres_fuites *child);
 
-
-//lecture fichier
-int detect_type(char* line); // 1 = source, 2 = usine, 3 = stockage, 4 = jonction, 5 = service, 6 = menages
+int detect_type(char *line);
 int code_len(const char *s);
 int empty(const char *s);
 
 
-void calcule_fuites(racine *usine);
-float recuperer_fuites(racine *usine);
-racine *ajouter_arbre_usine(racine *node, arbres_fuites *new);
+racine *ajouter_arbre_usine(racine *node, arbres_fuites *newn);
 racine *chercher_avl(const char *code_usine, arbre *root);
 
-// Traverse the AVL in-order and perform per-node action
+
+void calcule_fuites(racine *usine);
+float recuperer_fuites(racine *usine);
 void traverse_avl(arbre *root);
 
-#endif 
+#endif /* BASIC_FUNCTIONS_H_ */
